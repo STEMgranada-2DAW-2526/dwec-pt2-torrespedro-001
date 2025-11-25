e1f930938ec971e5805e164b80c6518a84a34825
Pedro <pedrotorres001.alu@stemgranada.com>
2025-11-25 12:09:14 +0100

    Añado cambios reducer WIP

--- PT2/src/components/GameProvider.jsx
+++ PT2/src/components/GameProvider.jsx
@@ -11,7 +11,7 @@ const EstadoJuego = {
   mejorasAutomaticas: 0,
   precioMultiplicador: 10,
   mejoras: {
-    canionSangriento: false,
+    canionTurron: false,
     renosLanzamisiles: false,
     arbolNavidadLaser: false,
   },
@@ -19,19 +19,19 @@ const EstadoJuego = {
 
 function calcularDanioMejoras(estado) {
   let total = 1;
-  if (estado.mejoras.canon) total += 2;
-  if (estado.mejoras.renos) total += 5;
-  if (estado.mejoras.arbol) total += 10;
+  if (estado.mejoras.canonTurron) total += 2;
+  if (estado.mejoras.renosLanzamisiles) total += 5;
+  if (estado.mejoras.arbolNavidadLaser) total += 10;
   return total;
 }
 
 function reductor(estado, accion) {
   if (accion.tipo === "DISPARAR") {
-    if (estado.danioActual < estado.danioObjetivo && estado.caramelos > 0) {
+    if (estado.danioActual < estado.danioObjetivo && estado.caramelosSangrientos > 0) {
       return {
         danioActual: estado.danioActual + calcularDanioMejoras(estado),
         danioObjetivo: estado.danioObjetivo,
-        caramelos: estado.caramelos - 1,
+        caramelosSangrientos: estado.caramelosSangrientos - 1,
         oleada: estado.oleada,
         mejoras: estado.mejoras,
       };
@@ -40,11 +40,11 @@ function reductor(estado, accion) {
   }
 
   if (accion.tipo === "AUTO_SHOOT") {
-    if (estado.danioAcumulado < estado.danioObjetivo && estado.caramelos > 0) {
+    if (estado.danioAcumulado < estado.danioObjetivo && estado.caramelosSangrientos > 0) {
       return {
         danioAcumulado: estado.danioAcumulado + calcularDanioPorDisparo(estado) * estado.disparosPorSegundo,
         danioObjetivo: estado.danioObjetivo,
-        caramelos: estado.caramelos - estado.disparosPorSegundo,
+        caramelosSangrientos: estado.caramelosSangrientos - estado.disparosPorSegundo,
         oleada: estado.oleada,
         disparosPorSegundo: estado.disparosPorSegundo,
         mejoras: estado.mejoras,
@@ -56,12 +56,33 @@ function reductor(estado, accion) {
   }
   
   if (accion.tipo === "BUY_MULTIPLIER") {
-    if (estado.caramelos >= estado.precioMultiplicador) {
+    if (estado.caramelosSangrientos >= estado.precioMultiplicador) {
       return {
         danioAcumulado: estado.danioAcumulado,
         danioObjetivo: estado.danioObjetivo,
-        caramelos: estado.caramelos - estado.precioMultiplicador,
+        caramelosSangrientos: estado.caramelosSangrientos - estado.precioMultiplicador,
         oleada: estado.oleada,
         disparosPorSegundo: estado.disparosPorSegundo + 1,
         mejoras: estado.mejoras,
-        mejorasAuto: 
+        mejorasAuto: estado.mejorasAuto + 1,
+        precioMultiplicador: Math.floor(estado.precioMultiplicador * 1.2),
+      };
+    }
+    return estado;
+  }
+
+  if (accion.tipo === "BUY_DAMAGE_UPGRADE") {
+    if (accion.nombre === "canonTurron" && !estado.mejoras.canonSangriento && estado.caramelos >= 15) {
+      return {
+        danioAcumulado: estado.danioAcumulado,
+        danioObjetivo: estado.danioObjetivo,
+        caramelosSangrientos: estado.caramelosSangrientos - 15,
+        oleada: estado.oleada,
+        disparosPorSegundo: estado.disparosPorSegundo,
+        mejoras: { canonTurron: true, renosLanzamisiles: estado.mejoras.renosLanzamisiles, arbolNavidadLaser: estado.mejoras.arbolNavidadLaser },
+        mejorasAuto: estado.mejorasAuto,
+        precioMultiplicador: estado.precioMultiplicador,
+      };
+    }
+
+

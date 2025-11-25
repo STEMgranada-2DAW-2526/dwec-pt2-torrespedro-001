df6d9787c5d3aa78e769224674bcf9a09fc70367
Pedro <pedrotorres001.alu@stemgranada.com>
2025-11-25 11:57:20 +0100

    Termino el auto shoot del reducer

--- PT2/src/components/GameProvider.jsx
+++ PT2/src/components/GameProvider.jsx
@@ -39,6 +39,19 @@ function reductor(estado, accion) {
     return estado;
   }
 
-  if(accion.tipo === "AUTO_SHOOT"){
-    if(estado.danioActual)
+  if (accion.tipo === "AUTO_SHOOT") {
+    if (estado.danioAcumulado < estado.danioObjetivo && estado.caramelos > 0) {
+      return {
+        danioAcumulado: estado.danioAcumulado + calcularDanioPorDisparo(estado) * estado.disparosPorSegundo,
+        danioObjetivo: estado.danioObjetivo,
+        caramelos: estado.caramelos - estado.disparosPorSegundo,
+        oleada: estado.oleada,
+        disparosPorSegundo: estado.disparosPorSegundo,
+        mejoras: estado.mejoras,
+        mejorasAuto: estado.mejorasAuto,
+        precioMultiplicador: estado.precioMultiplicador,
+      };
+    }
+    return estado;
   }
+  
